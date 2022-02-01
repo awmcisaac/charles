d01f6ebadc60f3f42229a87b9dc5104301a6d47c
Andy McIsaac <andrew.mcisaac@ntlworld.com>
2022-02-01 12:09:41 +0100

    npfl067 hw 1

--- npfl067/hw1/entropy.py
+++ npfl067/hw1/entropy.py
@@ -1,119 +1,124 @@
 import numpy as np
 import random
 from collections import Counter
-from functools import reduce
-from tqdm import tqdm
 
-def joint_probability(text):
-    ''' calculate joint probability P(i,j) '''
-    counts = Counter()
-    # get counts of pairs of words
-    for i in range(len(text)-1):
-        counts[text[i],text[i+1]] += 1
+class Entropy():
+    def __init__(self, text, lang):
+        # include start and end tags 
+        self.text = ['<s>'] + text + ['<s>']
+        self.lang = lang
 
-    total = sum(counts.values())
-    # work out probability based on counts (MLE)
-    probs = [val/total for val in counts.values()]
-
-    joint_probs = {k: v for k, v in zip(counts.keys(), probs)}
+    def joint_probability(self, text):
+        ''' calculate joint probability P(i,j) '''
+        counts = Counter()
+        # get counts of pairs of words
+        for i, j in zip(text, text[1:]):
+            counts[i, j] += 1
     
-    return joint_probs
-
-def conditional_probability(text):
-    ''' calculate conditional probability P(j|i) '''
-    # all words in the text have a dictionary for counts of following words
-    # e.g. {i: {j: n}}, where i is followed by j, and n is the number of times
-    # i is followed by j in the text
-    words = {word: Counter() for word in text}
-    for i in range(len(text)-1):
-        words[text[i]] += Counter([text[i+1]])
-
-    for word in words:
-        total = sum(words[word].values())
-        probs = [val/total for val in words[word].values()]
-        conditional_probs = {k: v for k, v in zip(words[word].keys(), probs)}
-        words[word] = conditional_probs
-
-    return words
-
-def get_entropy(text):
-    ''' H(J|I) = -sum(P(i,j) * log2(P(j|i)) '''
-    # calculate joint and conditional probabilities of text
-    joint_probs = joint_probability(text)
-    cond_probs = conditional_probability(text)
-
-    # calculate entropy from formula, using all i, j from joint_probs
-    # all other joint probabilities of words are 0, so not needed
-    entropy = 0
-    for (i, j), prob in joint_probs.items():
-        joint = prob
-        log_cond = np.log2(cond_probs[i][j])
-        entropy -= joint * log_cond
-
-    return entropy
-
-def mess_up_chars(text, likelihood):
-    # set of characters that appear in the text
-    charset = {char for word in text for char in word}
-
-    # go through characters in words and words in texts, altering
-    # characters with a given likelihood
-    messed_up = []
-    for word in text:
-        new_word = ''
-        for char in word:
-            if random.random() <= likelihood:
-                new_char = random.choice(list(charset))
-            else:
-                new_char = char
-            new_word += new_char
-        messed_up.append(new_word)
-    return messed_up
-
-def mess_up_words(text, likelihood):
-    # set of words that appear in the text
-    wordset = set(text)
-
-    # map a word to a random word from the text with the given likelihood,
-    # otherwise keep the current word for all words in the text
-    messed_up = list(map(lambda x: random.choice(list(wordset)) if 
-        random.random() <= likelihood else x, text))
-    return messed_up
-
-def run(lang):
-    with open("TEXT"+lang+"1.txt", "r", encoding="iso-8859-2") as f:
-        text = [word.strip() for word in f.readlines()]
+        total = sum(counts.values())
+        # work out probability based on counts
+        joint_probs = {k: v/total for k, v in counts.items()}
+        return joint_probs
     
-    # get entropy of original text
-    entropy = get_entropy(text)
-    perplexity = 2**entropy
-    with open("results"+lang+".txt", "a") as f:
-        f.write("TEXT\tENTROPY\tPERPLEXITY\n")
-        f.write("original\t"+str(entropy)+"\t"+str(perplexity)+"\n")
+    def conditional_probability(self, text):
+        ''' compute all conditional bigram probabilities P(j|i) '''
+        counter_2 = Counter()
+        for i, j in zip(text, text[1:]):
+            counter_2[i, j] += 1
+        c_1 = Counter()
+        # c_1(i) = sum_w(c_2(i,j))
+        for i, j in counter_2:
+            c_1[i] += counter_2[i, j]
     
-#    likelihoods = [0.1, 0.05, 0.01, 0.001, 0.0001, 0.00001]
-#    for likelihood in likelihoods:
-#        for i in range(10):
-#            messed_up = mess_up_chars(text, likelihood)
-#            entropy = get_entropy(messed_up)
-#            perplexity = 2**entropy
-#            with open("results"+lang+".txt", "a") as f:
-#                f.write("char_"+str(likelihood)+"\t"+str(entropy)+"\t"+str(perplexity)+"\n")
-#    for likelihood in likelihoods:
-#        for i in range(10):
-#            messed_up = mess_up_words(text, likelihood)
-#            entropy = get_entropy(messed_up)
-#            perplexity = 2**entropy
-#            with open("results"+lang+".txt", "a") as f:
-#                f.write("word_"+str(likelihood)+"\t"+str(entropy)+"\t"+str(perplexity)+"\n")
+        # P(j|i) = c_2(i,j) / c_1(i)
+        cond_probs = {(j, i): counter_2[i, j]/c_1[i] for i, j in counter_2}
+        return cond_probs
+    
+    def get_entropy(self, text):
+        ''' 
+        compute conditional entropy of text
+        H(J|I) = -sum(P(i,j) * log2(P(j|i))
+        '''
+        # calculate joint and conditional probabilities of text
+        joint_probs = self.joint_probability(text)
+        cond_probs = self.conditional_probability(text)
+    
+        # calculate entropy from formula, using all i, j from joint_probs
+        # all other joint probabilities of words are 0, so not needed
+        entropy = 0
+        for (i, j), prob in joint_probs.items():
+            joint = prob
+            log_cond = np.log2(cond_probs[j, i])
+            entropy -= joint * log_cond
+        return entropy
+    
+    def mess_up_chars(self, text, likelihood):
+        '''
+        go through words in the text and characters in words,
+        altering characters with a given likelihood
+        return the text with individual characters messed up
+        '''
+        # set of characters that appear in the text
+        char_set = {char for word in text for char in word}
+        messed_up = []
+        for word in text:
+            new_word = ''
+            for char in word:
+                if random.random() <= likelihood:
+                    new_char = random.choice(list(char_set))
+                else:
+                    new_char = char
+                new_word += new_char
+            messed_up.append(new_word)
+        return messed_up
+    
+    def mess_up_words(self, text, likelihood):
+        '''
+        map a word to a random word from the text with the given likelihood,
+        otherwise keep the current word for all words in the text
+        return the text with individual words messed up
+        '''
+        word_set = set(text)
+        messed_up = list(map(lambda x: random.choice(list(word_set)) if 
+            random.random() <= likelihood else x, text))
+        return messed_up
+
+    def write_to_file(self, label, entropy, perplexity):
+        with open('entropy_results'+self.lang+'.txt', 'a') as f:
+            f.write(label+'\t'+str(entropy)+'\t'+str(perplexity)+'\n')
+    
+    def run(self):
+        # get entropy of original text
+        entropy = self.get_entropy(self.text)
+        perplexity = 2**entropy
+        self.write_to_file('original', entropy, perplexity)
         
-#run("EN")
-#run("CZ")
-#run("COMBINED")
+        likelihoods = [0.1, 0.05, 0.01, 0.001, 0.0001, 0.00001]
 
-with open("TEXTCOMBINED.txt", "r", encoding="iso-8859-2") as f:
-    text = [word.strip() for word in f.readlines()]
-print(text[:7])
-print(text[7:])
+        for likelihood in likelihoods:
+            for i in range(10):
+                messed_up = self.mess_up_chars(self.text, likelihood)
+                entropy = self.get_entropy(messed_up)
+                perplexity = 2**entropy
+                self.write_to_file('char_'+str(likelihood), entropy, perplexity)
 
-run("COMBINED")
+        for likelihood in likelihoods:
+            for i in range(10):
+                messed_up = self.mess_up_words(self.text, likelihood)
+                entropy = self.get_entropy(messed_up)
+                perplexity = 2**entropy
+                self.write_to_file('word_'+str(likelihood), entropy, perplexity)
+        
+if __name__ == '__main__':
+    for lang in ['EN', 'CZ']:
+        with open('TEXT'+lang+'1.txt', 'r', encoding='iso-8859-2') as f:
+            text = [word.strip() for word in f.readlines()]
+
+        entropy = Entropy(text, lang)
+
+        # prepare results file
+        with open('entropy_results'+lang+'.txt', 'w') as f:
+            f.write('TEXT\tENTROPY\tPERPLEXITY\n')
+
+        # run experiments
+        entropy.run()
--- npfl067/hw1/lm.py
+++ npfl067/hw1/lm.py
@@ -1,180 +1,181 @@
 import numpy as np
-import pandas as pd
-from collections import Counter, defaultdict
+from collections import Counter
 
-#with open("TEXTEN1_test.txt", "w") as f:
-#    f.writelines([word+'\n' for word in test])
-#with open("TEXTEN1_heldout.txt", "w") as f:
-#    f.writelines([word+'\n' for word in holdout])
-#with open("TEXTEN1_train.txt", "w") as f:
-#    f.write lines([word+'\n' for word in train])
+class LM():
+    def __init__(self, text, lang):
+        # split text into train, heldout, and test sets
+        # prepend start 'words' to avoid issues at beginning of data
+        self.train = ['<s>', '<s>'] + text[:-60000]
+        self.heldout = ['<s>', '<s>'] + text[-60000:-20000]
+        self.test = ['<s>', '<s>'] + text[-20000:]
 
-def get_uniform_probs(text):
-    vocab_size = len(set(text))
-    p_0 = 1/vocab_size
-    return p_0
+        self.lang = lang
 
-def get_unigram_probs(text):
-    text_size = len(text)
-    counter_1 = Counter(text)
-    probs_1 = [count/text_size for count in counter_1.values()]
-    p_1 = {k: v for k, v in zip(counter_1.keys(), probs_1)}
-    return p_1
+        # initialise dictionaries for ngram probabilities and counts
+        self.p_0 = self.get_uniform_probs()
+        self.p_1 = self.get_unigram_probs()
+        self.p_2, self.c_1 = self.get_bigram_probs()
+        self.p_3, self.c_2 = self.get_trigram_probs()
 
-def get_bigram_probs(text):
-    ''' c_1(y) = sum_z(c_2(y,z)) '''
-    counter_2 = Counter()
-    for i in range(len(text)-1):
-        counter_2[text[i], text[i+1]] += 1
-    counter_1 = Counter()
-    for y, z in counter_2:
-        counter_1[y] += counter_2[y,z]
-
-    # p_2(w_i | w_i-1) = c_2(w_i-1, w_i) / c_1(w_i-1)
-    p_2 = {(z,y): counter_2[y,z]/counter_1[y] for y, z in counter_2}
-    return p_2, counter_1
-
-def get_trigram_probs(text):
-    ''' c_2(y,z) = sum_w(c_3(y,z,w)) '''
-    counter_3 = Counter()
-    for i in range(len(text)-2):
-        counter_3[text[i], text[i+1], text[i+2]] += 1
-    counter_2 = Counter()
-    for y, z, w in counter_3:
-        counter_2[y,z] += counter_3[y,z,w]
-
-    # p_3(w_i | w_i-2, w_i-1) = c_3(w_i-2, w_i-1, w_i) / c_2(w_i-2, w_i-1)
-    p_3 = {(w,y,z): counter_3[y,z,w]/counter_2[y,z] for y, z, w in counter_3}
-    return p_3, counter_2
-
-def compute_smoothing(p, heldout, epsilon=0.01):
-    # randomly initialise l and normalise
-    l = np.random.rand(4)
-    l = l/np.sum(l)
-
-    # prepend start 'words' to avoid issues at beginning of data
-    heldout = ['<s>', '<s>'] + heldout
-
-    def em(l):
-        p_smooth = defaultdict(int)
-        # start at 2, since 0, 1 are start words
-        for i in range(2, len(heldout)):
-            p_smooth[heldout[i], heldout[i-2], heldout[i-1]] \
-                    += l[3]*p[3].get((heldout[i], heldout[i-2], heldout[i-1]),
-                        0 if counter_2[heldout[i-2], heldout[i-1]] > 0 else p[0]) \
-                    + l[2]*p[2].get((heldout[i], heldout[i-1]),
-                        0 if counter_1[heldout[i-1]] > 0 else p[0]) \
-                    + l[1]*p[1].get((heldout[i]), 0) \
-                    + l[0]*p[0]
+    def get_uniform_probs(self):
+        ''' compute uniform probability '''
+        # p_0(w_i) = 1/V
+        return 1/len(set(self.train))
     
-        counts = np.zeros(4)
-        for i in range(2, len(heldout)):
-            counts[0] += (l[0]*p[0])/p_smooth[heldout[i], heldout[i-2], heldout[i-1]]
+    def get_unigram_probs(self):
+        ''' compute all unigram probabilities '''
+        text_size = len(self.train)
+        counter_1 = Counter(self.train)
+        # p_1(w_i) = c_1(w_1)/T
+        p_1 = {word: count/text_size for word, count in counter_1.items()}
+        return p_1
     
-        for i in range(2, len(heldout)):
-            counts[1] += (l[1]*p[1].get((heldout[i]), 0)) \
-                    /p_smooth[heldout[i], heldout[i-2], heldout[i-1]]
+    def get_bigram_probs(self):
+        ''' 
+        compute all conditional bigram probabilities
+        return bigram probabilities and count of all words in the history
+        '''
+        counter_2 = Counter()
+        # for each bigram
+        for z, w in zip(self.train, self.train[1:]):
+            counter_2[z, w] += 1
+        c_1 = Counter()
+        # c_1(z) = sum_w(c_2(z,w))
+        for z, w in counter_2:
+            c_1[z] += counter_2[z, w]
     
-        for i in range(2, len(heldout)):
-            counts[2] += (l[2]*p[2].get((heldout[i], heldout[i-1]), 0)) \
-                    /p_smooth[heldout[i], heldout[i-2], heldout[i-1]]
+        # p_2(w_i | w_i-1) = c_2(w_i-1, w_i) / c_1(w_i-1)
+        p_2 = {(w, z): counter_2[z, w]/c_1[z] for z, w in counter_2}
+        return p_2, c_1
     
-        for i in range(2, len(heldout)):
-            counts[3] += (l[3]*p[3].get((heldout[i], heldout[i-2], heldout[i-1]), 0)) \
-                    /p_smooth[heldout[i], heldout[i-2], heldout[i-1]]
+    def get_trigram_probs(self):
+        '''
+        compute all conditional trigram probabilities
+        return trigram probabilities and count of all words in the history
+        '''
+        counter_3 = Counter()
+        # for each trigram
+        for y,z,w in zip(self.train, self.train[1:], self.train[2:]):
+            counter_3[y, z, w] += 1
+        c_2 = Counter()
+        # c_2(y,z) = sum_w(c_3(y,z,w))
+        for y,z,w in counter_3:
+            c_2[y, z] += counter_3[y, z, w]
     
-        # next l
-        next_l = counts/np.sum(counts)
+        # p_3(w_i | w_i-2, w_i-1) = c_3(w_i-2, w_i-1, w_i) / c_2(w_i-2, w_i-1)
+        p_3 = {(w, y, z): counter_3[y, z, w]/c_2[y, z] for y, z, w in counter_3}
+        return p_3, c_2
+
+    def compute_smoothing(self, epsilon):
+        '''
+        run EM algorithm on heldout data with stopping condition
+        |l_j - l_j,next| < epsilon for each j
+        '''
+        # randomly initialise l and normalise
+        l = np.random.rand(4)
+        l /= np.sum(l)
     
-        if (np.abs(l - next_l) < epsilon).all():
-            return p_smooth, next_l
-        else:
-            return em(next_l)
-
-    return em(l)
-
-def tweaked_smoothing(p, heldout, l):
-    # prepend start 'words' to avoid issues at beginning of data
-    heldout = ['<s>', '<s>'] + heldout
-    p_smooth = defaultdict(int)
-    # start at 2, since 0, 1 are start words
-    for i in range(2, len(heldout)):
-        p_smooth[heldout[i], heldout[i-2], heldout[i-1]] \
-                += l[3]*p[3].get((heldout[i], heldout[i-2], heldout[i-1]),
-                    0 if counter_2[heldout[i-2], heldout[i-1]] > 0 else p[0]) \
-                + l[2]*p[2].get((heldout[i], heldout[i-1]),
-                    0 if counter_1[heldout[i-1]] > 0 else p[0]) \
-                + l[1]*p[1].get((heldout[i]), 0) \
-                + l[0]*p[0]
-    return p_smooth
-
-
-def get_cross_entropy(p_smooth, test):
-    ''' 
-    compute cross entropy of test data
-    -1/|T'| sum(log2(P(yi|xi)))
-    '''
-    test = ['<s>', '<s>'] + test
-
-    cross_entropy = 0
-    for i in range(2, len(test)):
-        cross_entropy += np.log2(p_smooth.get((test[i], test[i-2], test[i-1]),
-            1/vocab_size))
-
-    return -1/len(test) * cross_entropy
-
-
-def run(lang):
-    # compute smoothing parameters and smoothed language model
-    p_smooth, l = compute_smoothing(p, heldout, epsilon=1e-12)
-    # compute cross entropy of smoothed language model on test data
-    cross_entropy = get_cross_entropy(p_smooth, test)
-
-    with open("lm_results"+lang+".txt", "a") as f:
-        f.write("TEXT\tLAMBDAS\tCROSS ENTROPY\n")
-        f.write("original\t"+str(l)+"\t"+str(cross_entropy)+"\n")
-
-    tweaked_l = np.zeros(4)
+        def em(l):
+            exp_counts = np.zeros(4)
+            for y,z,w in zip(self.heldout, self.heldout[1:], self.heldout[2:]):
+                curr_counts = np.zeros(4)
+                # compute expected counts of each lambda
+                curr_counts[0] += l[0]*self.p_0
+                curr_counts[1] += l[1]*self.p_1.get((w), 0)
+                # controlling counts as described on slide 93
+                curr_counts[2] += l[2]*self.p_2.get((w, z),
+                        0 if self.c_1[z] > 0 else self.p_0)
+                curr_counts[3] += l[3]*self.p_3.get((w, y, z),
+                        0 if self.c_2[y,z] > 0 else self.p_0)
+                
+                # divide by smoothed probability
+                curr_counts /= np.sum(curr_counts)
+                # add to total expected counts
+                exp_counts += curr_counts
+        
+            # next l (normalise)
+            next_l = exp_counts/np.sum(exp_counts)
+        
+            # check stopping condition, else repeat with next l
+            if (np.abs(l - next_l) < epsilon).all():
+                return next_l
+            else:
+                return em(next_l)
     
-    percentage_tweaks = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99]
-    for tweak in percentage_tweaks:
-        trigram_diff = 1 - l[3]
-        tweaked_l[3] = l[3] + trigram_diff * tweak
-        tweaked_l[0:3] = l[0:3] - (l[0:3]/np.sum(l[0:3])) * trigram_diff * tweak
-        tweaked_p_smooth = tweaked_smoothing(p, heldout, tweaked_l)
-        cross_entropy = get_cross_entropy(tweaked_p_smooth, test)
-        with open("lm_results"+lang+".txt", "a") as f:
-            f.write("add_"+str(tweak)+"\t"+str(tweaked_l)+"\t"
-                    +str(cross_entropy)+"\n")
+        return em(l)
+    
+    def get_smoothed_prob(self, l, w, y, z):
+        '''
+        compute smoothed probability ps(w|y,z) as
+        defined in the assignment brief
+        return ps(w|y,z)
+        '''
+        ps_0 = l[0] * self.p_0
+        ps_1 = l[1] * self.p_1.get((w), 0)
+        ps_2 = l[2] * self.p_2.get((w,z),
+                0 if self.c_1[z] > 0 else self.p_0)
+        ps_3 = l[3] * self.p_3.get((w, y, z), 
+                0 if self.c_2[y,z] > 0 else self.p_0)
+        p_smooth = ps_0 + ps_1 + ps_2 + ps_3
+        return p_smooth
+    
+    def get_cross_entropy(self, l):
+        ''' 
+        compute cross entropy of all test data T'
+        return -1/|T'| sum(log2(ps(w|y,z)))
+        '''
+        cross_entropy = 0
+        for y,z,w in zip(self.test, self.test[1:], self.test[2:]):
+            cross_entropy += np.log2(self.get_smoothed_prob(l, w, y, z))
+    
+        return -1/len(self.test) * cross_entropy
 
-    discount_tweaks = [0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0]
-    for tweak in discount_tweaks:
-        tweaked_l[3] = l[3] * tweak
-        trigram_diff = l[3] - tweaked_l[3]
-        tweaked_l[0:3] = l[0:3] + (l[0:3]/np.sum(l[0:3])) * trigram_diff
-        tweaked_p_smooth = tweaked_smoothing(p, heldout, tweaked_l)
-        cross_entropy = get_cross_entropy(tweaked_p_smooth, test)
-        with open("lm_results"+lang+".txt", "a") as f:
-            f.write("discount_"+str(tweak)+"\t"+str(tweaked_l)+"\t"
-                    +str(cross_entropy)+"\n")
+    def write_to_file(self, label, l, cross_entropy):
+        ''' save results '''
+        with open('lm_results'+self.lang+'.txt', 'a') as f:
+            f.write(label+'\t'+str(l)+'\t'+str(cross_entropy)+'\n')
+
+    def run(self):
+        # compute smoothing parameters
+        l = self.compute_smoothing(epsilon=1e-5)
+        # compute cross entropy of smoothed language model on test data
+        cross_entropy = self.get_cross_entropy(l)
+        self.write_to_file('original', l, cross_entropy)
+    
+        tweaked_l = np.zeros(4)
+        percentage_tweaks = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99]
+
+        for tweak in percentage_tweaks:
+            trigram_diff = 1 - l[3]
+            # add x% of the difference between l[3] and 1 to tweaked_l[3]
+            tweaked_l[3] = l[3] + trigram_diff * tweak
+            # discount remaining parameters proportionally to sum to 1
+            tweaked_l[0:3] = l[0:3] - (l[0:3]/np.sum(l[0:3])) \
+                    * trigram_diff * tweak
+            cross_entropy = self.get_cross_entropy(tweaked_l)
+            self.write_to_file('add_'+str(tweak), tweaked_l, cross_entropy)
+    
+        discount_tweaks = [0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0]
+
+        for tweak in discount_tweaks:
+            tweaked_l[3] = l[3] * tweak
+            # discount x% of l[3]
+            trigram_diff = l[3] - tweaked_l[3]
+            # boost remaining parameters proportionally to sum to 1
+            tweaked_l[0:3] = l[0:3] + (l[0:3]/np.sum(l[0:3])) * trigram_diff
+            cross_entropy = self.get_cross_entropy(tweaked_l)
+            self.write_to_file('discount_'+str(tweak), tweaked_l, cross_entropy)
 
 if __name__ == '__main__':
-    for lang in ["EN", "CZ"]:
-        with open("TEXT"+lang+"1.txt", "r", encoding="iso-8859-2") as f:
+    for lang in ['EN', 'CZ']:
+        with open('TEXT'+lang+'1.txt', 'r', encoding='iso-8859-2') as f:
             text = [word.strip() for word in f.readlines()]
-        train = text[:-60000]
-        heldout = text[-60000:-20000]
-        test = text[-20000:]
-    
-        vocab = set(train)
-        vocab_size = len(vocab)
-        text_size = len(train)
-    
-        # calculate probabilities from training data
-        p_0 = get_uniform_probs(train)
-        p_1 = get_unigram_probs(train)
-        p_2, counter_1 = get_bigram_probs(train)
-        p_3, counter_2 = get_trigram_probs(train)
-        p = [p_0, p_1, p_2, p_3]
+        
+        lm = LM(text, lang)
 
-        run(lang)
+        # prepare results file
+        with open('lm_results'+lang+'.txt', 'w') as f:
+            f.write('TEXT\tLAMBDAS\tCROSS ENTROPY\n')
+
+        # run experiments
+        lm.run()
